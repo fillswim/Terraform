c4286d134d669b7e3d083e5aa2bccfcfaa36d094
Oleg Filatov <fillswim@gmail.com>
2024-10-21 14:42:22 +0300

    Update module source

--- test-module-ubuntu/test-module-ubuntu.tf
+++ test-module-ubuntu/test-module-ubuntu.tf
@@ -4,7 +4,7 @@ module "test-ubuntu" {
     proxmox = proxmox.my_proxmox
   }
 
-  source = "../modules/ubuntu"
+  source = "github.com/fillswim/Terraform/modules/ubuntu"
 
   count_vms = 1
   # octet3 - vlan
