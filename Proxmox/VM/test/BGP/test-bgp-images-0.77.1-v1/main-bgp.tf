e9d6a88a1c49aefdf0e4436e0725370d1c66bf6a
Oleg Filatov <fillswim@gmail.com>
2025-05-15 16:48:35 +0300

    Update

--- Proxmox/VM/test/BGP/test-bgp-images-0.77.1-v1/main-bgp.tf
+++ Proxmox/VM/test/BGP/test-bgp-images-0.77.1-v1/main-bgp.tf
@@ -5,6 +5,6 @@ resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
   content_type = "iso"
   datastore_id = "local"
   url          = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
-  file_name    = "Ubuntu-24.04-LTS-cloudimg-amd64.img"
+  file_name    = var.image_name
 }
 
--- Proxmox/VM/test/BGP/test-bgp-images-0.77.1-v1/test-bgp-images.tf
+++ Proxmox/VM/test/BGP/test-bgp-images-0.77.1-v1/test-bgp-images.tf
@@ -2,3 +2,9 @@ variable "count_proxmox_nodes" {
   type    = number
   default = 5
 }
+
+variable "image_name" {
+  type    = string
+}
+
+
