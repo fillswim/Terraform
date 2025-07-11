3d53a93b96692a0eae07dc8ceb8fec4a4dcba2a3
Oleg Filatov <fillswim@gmail.com>
2025-07-11 12:55:35 +0300

    Add Helm release

---
+++ Kubernetes/K8s1-Alma/Cilium/helm-release-cilium.tf
@@ -0,0 +1,15 @@
+# =====================================================
+#                        Cilium
+# =====================================================
+resource "helm_release" "cilium" {
+  name       = "cilium"
+  repository = "https://helm.cilium.io"
+  chart      = "cilium"
+  version    = "1.17.5"
+  namespace  = "kube-system"
+
+  values = [
+    file("${path.module}/cilium-my-values-1.17.5.yaml")
+  ]
+}
+# =====================================================
