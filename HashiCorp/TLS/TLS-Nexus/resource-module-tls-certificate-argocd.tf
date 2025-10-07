c9ed4c4e893dd94c7314c666ce8f4c97ae33032b
Oleg Filatov <fillswim@gmail.com>
2025-10-07 17:57:17 +0300

    Enhance TLS configuration with Cert-Manager integration

---
+++ HashiCorp/TLS/TLS-Nexus/resource-module-tls-certificate-argocd.tf
@@ -0,0 +1,40 @@
+
+
+
+# ==============================================================================
+#                              Сертификат, подписанный CA
+# ==============================================================================
+
+module "nexus_certificate" {
+
+  source = "/home/fill/Terraform-Modules/HashiCorp/TLS/CA-Self-Signed/v1"
+
+  algorithm             = var.algorithm
+  rsa_bits              = var.rsa_bits
+  common_name           = var.common_name
+  organization          = var.organization
+  dns_names             = var.dns_names
+  # ca_private_key_pem    = file(var.ca_private_key_pem_path)
+  # ca_cert_pem           = file(var.ca_cert_pem_path)
+  ca_private_key_pem    = data.terraform_remote_state.remote_state_tls_ca_certificate.outputs.ca_private_key_pem
+  ca_cert_pem           = data.terraform_remote_state.remote_state_tls_ca_certificate.outputs.ca_cert_pem
+
+}
+
+# ==============================================================================
+#                             Самоподписанный сертификат
+# ==============================================================================
+
+# module "argocd_certificate" {
+
+#   source = "/home/fill/Terraform-Modules/HashiCorp/TLS/Simple-Self-Signed/v1"
+
+#   algorithm             = var.algorithm
+#   rsa_bits              = var.rsa_bits
+#   common_name           = var.common_name
+#   organization          = var.organization
+#   dns_names             = var.dns_names
+
+# }
+
+# # ==============================================================================
