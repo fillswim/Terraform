d062be603c032acee134832540cc8894422c4aa1
Oleg Filatov <fillswim@gmail.com>
2026-01-13 13:43:45 +0300

    Update

---
+++ Proxmox/VM/test/BGP/module/download_files/images_ubuntu_22.04/main_images_ubuntu_22_04.tf
@@ -0,0 +1,13 @@
+module "Ubuntu_22_04" {
+
+  source = "/home/fill/Terraform-Modules/Proxmox/bpg/0.77.1/v2/download_file"
+
+  proxmox_node_name = var.proxmox_node_name
+  image_url         = var.image_url
+  image_name        = var.image_name
+  image_type        = var.image_type
+}
+
+output "details" {
+  value = module.Ubuntu_22_04.details
+}
---
+++ Proxmox/VM/test/BGP/module/download_files/images_ubuntu_22.04/variables.tf
@@ -0,0 +1,18 @@
+
+variable "proxmox_node_name" {
+  type    = string
+}
+
+variable "image_url" {
+  type    = string
+}
+
+variable "image_name" {
+  type    = string
+}
+
+variable "image_type" {
+  type    = string
+}
+
+
